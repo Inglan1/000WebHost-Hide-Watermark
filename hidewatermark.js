87c488bde80ae970597dadca30b7426c1591d150
Inglan1 <100646327+Inglan1@users.noreply.github.com>
2022-07-24 11:23:14 +1000

    Create hidewatermark.js

---
+++ hidewatermark.js
@@ -0,0 +1,6 @@
+var numelems = document.getElementsByTagName("div").length;
+var numelemsmin = numelems - 1;
+document.getElementsByTagName("div")[numelemsmin].remove();
+var numelems = document.getElementsByTagName("script").length;
+var numelemsmin = numelems - 1;
+document.getElementsByTagName("div")[numelemsmin].remove()
